a316b2d90b0478d980d7b489838ee879b925cb4e
E. A. Harris <mal@gahan-corporation.com>
2017-04-17 22:09:10 +0000

    Updated file locations. See #15.

---
+++ python/users.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+class ProcessUsers(ab.DoFn):
+    def process(self, element):
+        key, values = element
+        users = values.get('users')[0]
+        for k, v in enumerate(values.get('streams')):
+            values.get('streams')[k].update({
+                'product':users.get('product'),
+                'country':users.get('country'),
+                'region':users.get('region'),
+                'zip_code':users.get('zip_code'),
+                'access':users.get('access'),
+                'gender':users.get('gender'),
+                'partner':users.get('partner'),
+                'referral':users.get('referral'),
+                'type':users.get('type'),
+                'birth_year':users.get('birth_year')
+            })
+        # values.get('streams').pop(0)
+        return values.get('streams')
