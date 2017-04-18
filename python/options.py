861cc5599e1f1ab80320f674865b327b18232db3
E. A. Harris <mal@gahan-corporation.com>
2017-04-18 02:04:02 +0000

    Setting options has all the tests written anyway. See #22.

--- python/options.py
+++ python/options.py
@@ -12,15 +12,15 @@ class SetPipelineOptions():
         self.google_cloud = init_config.get("google_cloud")
         self.standard = init_config.get("standard")
 
-    def set_pipeline(self):
-        return apache_beam.utils.pipeline_options.PipelineOptions
-
-    def set_google_cloud_options(self, google_cloud):
-        options = self.view_as(apache_beam.utils.pipeline_options.GoogleCloudOptions) 
+    def set_google_cloud_options(self, google_cloud, pipeline):
+        options = pipeline.view_as(apache_beam.utils.pipeline_options.GoogleCloudOptions) 
         options.project = google_cloud.get("project") 
         options.staging_location = google_cloud.get("staging") 
         options.temp_location = google_cloud.get("temp") 
         return options 
+
+    def set_pipeline(self):
+        return apache_beam.utils.pipeline_options.PipelineOptions
 
     def set_runner(self, options, runner):
         options = options.view_as(ab.utils.pipeline_options.StandardOptions)
