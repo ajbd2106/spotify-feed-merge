ca964d0a38875a3bdef45e86f7d250366ddd7094
E. A. Harris <mal@gahan-corporation.com>
2017-04-18 01:23:17 +0000

    This will execute at least. See #22.

--- python/options.py
+++ python/options.py
@@ -4,23 +4,23 @@ import apache_beam
 import configobj
 import os
 
+class SetPipelineOptions():
+    config = configobj.ConfigObj(os.getcwd()+"/sfm.conf")
 
-class SetOptions:
     def __init__(self):
-        config = configobj.ConfigObj(os.getcwd()+"/sfm.conf")
-        google_cloud = configobj.get("google_cloud")
-        runner = configobj.get("standard").get("runner")
-        options = apache_beam.utils.pipeline_options.PipelineOptions()
-        options = set_google_cloud_options(options, google_cloud)
-        options = set_runner(options, runner)
-        return options 
+        init_config = SetPipelineOptions.config
+        self.google_cloud = init_config.get("google_cloud")
+        self.standard = init_config.get("standard")
 
-    def set_google_cloud_options(self, options, google_cloud):
-        google_cloud_options = options.view_as(apache_beam.utils.pipelne_options.GoogleCloudOptions) 
-        google_cloud_options.project = google_cloud.get("project") 
-        google_cloud_options.staging_location = google_cloud.get("staging") 
-        google_cloud_options.temp_location = google_cloud.get("temp") 
-        return google_cloud_options
+    def set_pipeline(self):
+        return apache_beam.utils.pipeline_options.PipelineOptions
+
+    def set_google_cloud_options(self, google_cloud):
+        options = self.view_as(apache_beam.utils.pipeline_options.GoogleCloudOptions) 
+        options.project = google_cloud.get("project") 
+        options.staging_location = google_cloud.get("staging") 
+        options.temp_location = google_cloud.get("temp") 
+        return options 
 
     def set_runner(self, options, runner):
         options = options.view_as(ab.utils.pipeline_options.StandardOptions)
