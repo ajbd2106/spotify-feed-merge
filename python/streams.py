3e1cea6e84e19068ff8d8da4c043eab2ee3de7ee
E. A. Harris <mal@gahan-corporation.com>
2017-04-18 19:54:16 +0000

    Added pipeline. See #25.

--- python/streams.py
+++ python/streams.py
@@ -7,9 +7,11 @@ class ReadStreams:
         pass
 
     def read_streams(self, pipeline):
-        return pipeline 
-             | 'read streams' >> apache_beam.io.ReadFromText('gs://abbynormal/streams.gz') 
+        return (pipeline 
+             | 'read streams' >> apache_beam.io.ReadFromText('gs://abbynormal/streams.gz')
+        ) 
 
     def map_steams(self, pipeline):
-        return pipeline
+        return (pipeline
              | 'map streams' >> apache_beam.Map(lambda user_id: (json.loads(user_id).get('user_id'), json.loads(user_id)))
+        )
