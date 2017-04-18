e7bd5bf63554ec15e9ee45a2d9d56c2aa068375d
E. A. Harris <mal@gahan-corporation.com>
2017-04-18 19:47:26 +0000

    Added methods to class. See #25.

--- python/streams.py
+++ python/streams.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import apache_beam
+
 class ReadStreams:
-    def __init__():
+    def __init__(self):
         pass
 
+    def read_streams(self, pipeline):
+        return pipeline 
+             | 'read streams' >> apache_beam.io.ReadFromText('gs://abbynormal/streams.gz') 
+
+    def map_steams(self, pipeline):
+        return pipeline
+             | 'map streams' >> apache_beam.Map(lambda user_id: (json.loads(user_id).get('user_id'), json.loads(user_id)))
