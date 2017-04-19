34448b05e3fbca4534cfaa598118d5104e72298c
E. A. Harris <mal@gahan-corporation.com>
2017-04-19 04:02:16 +0000

    If this passes I'm done for the day. See #25.:

--- python/streams.py
+++ python/streams.py
@@ -7,7 +7,7 @@ import os
 from options import SetPipelineOptions
 
 class ReadStreams:
-    streams_path = configobj.ConfigObj("/python/sfm.conf").get('standard').get('streams')    
+    streams_path = configobj.ConfigObj("sfm.conf").get('standard').get('streams')    
 
     def read_streams(self, pipeline):
         return (pipeline 
