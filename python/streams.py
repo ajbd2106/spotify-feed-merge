060b3ed7a0fcb895287d269b600d2f6aa4d50298
E. A. Harris <mal@gahan-corporation.com>
2017-04-20 22:05:04 +0000

    Gonna need to test this. See #34.

--- python/streams.py
+++ python/streams.py
@@ -17,3 +17,8 @@ class ReadStreams:
         return (pipeline
              | 'map streams' >> apache_beam.Map(lambda user_id: (json.loads(user_id).get('user_id'), json.loads(user_id)))
         )
+
+class GroupStreamsWithUsers:
+    def group_by_user_id(self, pipeline):
+        #(({'streams':s,'users':u}) | 'co group by key users' >> ab.CoGroupByKey())
+        return None
