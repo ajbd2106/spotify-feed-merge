2b2e2aa6bd65f8e93b961ef26517dc060f230a0c
E. A. Harris <mal@gahan-corporation.com>
2017-04-20 22:13:09 +0000

    Running and failing. See #34.

--- python/streams.py
+++ python/streams.py
@@ -19,6 +19,6 @@ class ReadStreams:
         )
 
 class GroupStreamsWithUsers:
-    def group_by_user_id(self, streams, users):
+    def group_streams_with_users(self, streams, users):
         #(({'streams':s,'users':u}) | 'co group by key users' >> ab.CoGroupByKey())
         return None
