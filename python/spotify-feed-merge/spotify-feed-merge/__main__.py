bb8da6d908a11cf63c1a5d0c7875be1b30d8e539
E. A. Harris <mal@gahan-corporation.com>
2017-04-24 22:29:18 +0000

    Users finally. See #42.

--- python/spotify-feed-merge/spotify-feed-merge/__main__.py
+++ python/spotify-feed-merge/spotify-feed-merge/__main__.py
@@ -52,6 +52,42 @@ class ProcessTracks(apache_beam.DoFn):
             })
         return values.get('streams') 
 
+class ProcessUsers(apache_beam.DoFn):
+    def process(self, element):
+        key, values = element
+        users = values.get('users')[0]
+        for k, v in enumerate(values.get('streams')):
+            values.get('streams')[k].update({
+                'product':users.get('product'),
+                'country':users.get('country'),
+                'region':users.get('region'),
+                'zip_code':users.get('zip_code'),
+                'access':users.get('access'),
+                'gender':users.get('gender'),
+                'partner':users.get('partner'),
+                'referral':users.get('referral'),
+                'type':users.get('type'),
+                'birth_year':users.get('birth_year')
+            })
+        # values.get('streams').pop(0)
+        return values.get('streams')
+
+
+class ReadUsers:
+    try:
+        users_path = configobj.ConfigObj("sfm/sfm.conf").get('standard').get('users') 
+    except:
+        users_path = configobj.ConfigObj("sfm.conf").get('standard').get('users') 
+
+    def read_users(self, pipeline):
+        return (pipeline
+            | 'read users' >> apache_beam.io.ReadFromText(self.users_path)
+        )
+    
+    def map_users(self, pipeline):
+        return (pipeline
+            | 'map users' >> apache_beam.Map(lambda user_id: (json.loads(user_id).get('user_id'), json.loads(user_id)))
+        )
 class ReadTracks:
     try:
         tracks_path = configobj.ConfigObj("sfm/sfm.conf").get('standard').get('tracks')
--- python/spotify-feed-merge/spotify-feed-merge/tracks.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import apache_beam
-import json
-import configobj
-
-from options import SetPipelineOptions
